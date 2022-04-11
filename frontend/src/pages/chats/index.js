5a6b4eb7fe954d0e5b0f8f6dddfbe01bd4a28307
Adam La Morre <alamorre@gmail.com>
2022-04-10 17:32:53 -0700

    Chat engine in frontend

--- frontend/src/pages/chats/index.js
+++ frontend/src/pages/chats/index.js
@@ -4,8 +4,21 @@ import { Context } from "../../context";
 
 import Header from "./Header";
 
+import {
+  MultiChatWindow,
+  useMultiChatLogic,
+  MultiChatSocket,
+} from "react-chat-engine-advanced";
+const projectId = "70049943-b572-4372-9f3c-fbdeca940e0f";
+
 const ChatsPage = () => {
   const { user } = useContext(Context);
+
+  const chatProps = useMultiChatLogic(
+    projectId,
+    user.username,
+    user.hashed_password
+  );
 
   if (!user) {
     return <div />;
@@ -13,7 +26,12 @@ const ChatsPage = () => {
     return (
       <div>
         <Header />
-        {user.first_name}'s chats...
+
+        <MultiChatSocket {...chatProps} />
+        <MultiChatWindow
+          {...chatProps}
+          style={{ height: "calc(100vh - 64px)" }}
+        />
       </div>
     );
   }
