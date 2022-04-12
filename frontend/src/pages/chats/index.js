c441740b93e25ae429e302a5f8bdf69540dffdb1
Adam La Morre <alamorre@gmail.com>
2022-04-12 16:35:24 -0700

    Ready for new r-c-e-a version

--- frontend/src/pages/chats/index.js
+++ frontend/src/pages/chats/index.js
@@ -24,14 +24,72 @@ const ChatsPage = () => {
     return <div />;
   } else {
     return (
-      <div>
+      <div style={{ backgroundColor: "#e8e5f4" }}>
         <Header />
 
-        <MultiChatSocket {...chatProps} />
-        <MultiChatWindow
-          {...chatProps}
-          style={{ height: "calc(100vh - 64px)" }}
-        />
+        <div
+          style={{ height: "calc(100vh - 64px)", backgroundColor: "#e8e5f4" }}
+        >
+          <div
+            style={{
+              position: "absolute",
+              top: "84px",
+              left: "-84px",
+              width: "324px",
+              height: "324px",
+              borderRadius: "50%",
+              backgroundColor: "#f0aa86",
+            }}
+          />
+          <div
+            style={{
+              position: "absolute",
+              top: "84px",
+              left: "184px",
+              width: "24px",
+              height: "24px",
+              borderRadius: "50%",
+              backgroundColor: "#f0aa86",
+            }}
+          />
+          <div
+            style={{
+              position: "absolute",
+              top: "124px",
+              left: "224px",
+              width: "94px",
+              height: "94px",
+              borderRadius: "50%",
+              backgroundColor: "#f0aa86",
+            }}
+          />
+          <div
+            style={{
+              position: "absolute",
+              bottom: "30px",
+              right: "70px",
+              width: "94px",
+              height: "94px",
+              borderRadius: "50%",
+              backgroundColor: "#f0aa86",
+            }}
+          />
+
+          <MultiChatSocket {...chatProps} />
+          <MultiChatWindow
+            {...chatProps}
+            style={{
+              position: "absolute",
+              top: "calc(10vh + 64px)",
+              left: "10vw",
+              height: "calc(80vh - 64px)",
+              width: "80vw",
+              border: "1px solid black",
+              boxShadow:
+                "rgba(50, 50, 93, 0.25) 0px 50px 100px -20px, rgba(0, 0, 0, 0.3) 0px 30px 60px -30px, rgba(10, 37, 64, 0.35) 0px -2px 6px 0px inset",
+            }}
+          />
+        </div>
       </div>
     );
   }
